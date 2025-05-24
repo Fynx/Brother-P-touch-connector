df9623c6bd43cf4f142be34f5fd09c77cb4ea7d5
Piotr Majcherczyk <pmv@mm.st>
2025-05-24 15:01:32 +0200

    Simplify status requesting

--- manage.py
+++ manage.py
@@ -1,10 +1,10 @@
+#!/usr/bin/python3
+
 import subprocess
 from sys import stdin
 from time import sleep
 
-status_request_path = "/tmp/status_request.bin"
 device_path = "/dev/usb/lp1"
-status_path = "/tmp/status_output.bin"
 
 subprocess.check_output(["./wr", "initialise", "-o", device_path])
 
@@ -16,8 +16,7 @@ for i in range(0, attempts):
 	sleep(5)
 
 	try:
-		subprocess.check_output(f"cat {device_path} > {status_path}", shell=True)
-		output = subprocess.check_output(["./read_status", "-i", status_path]).decode("utf-8")
+		output = subprocess.check_output(["./read_status", "-i", device_path]).decode("utf-8")
 		break
 	except subprocess.CalledProcessError:
 		print("failed")
