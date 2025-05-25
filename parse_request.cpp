cc418b5acf16935df1f9378244dca7c1a0fc1b2a
Piotr Majcherczyk <pmv@mm.st>
2025-05-25 20:20:58 +0200

    Parse more info

--- parse_request.cpp
+++ parse_request.cpp
@@ -22,10 +22,15 @@ struct SelectCompressionMode {
 	};
 };
 
+inline uint32_t ctou(char c)
+{
+	return static_cast<uint32_t>(static_cast<uint8_t>((c)));
+}
+
 inline void printHex(char c, bool asciiart = false)
 {
 	if (!asciiart) {
-		std::cerr << std::setfill('0') << std::hex << std::setw(2) << static_cast<uint32_t>(static_cast<uint8_t>((c)));
+		std::cerr << std::setfill('0') << std::hex << std::setw(2) << ctou(c);
 		return;
 	}
 
@@ -102,7 +107,7 @@ void parse(char *buf, unsigned len, uint32_t flags)
 			return;
 		}
 
-		std::cerr << "> " << std::hex << std::setfill('0') << std::setw(2) << "0x" << static_cast<uint32_t>(static_cast<uint8_t>(buf[i])) << "\n";
+		std::cerr << "> " << std::hex << std::setfill('0') << std::setw(2) << "0x" << ctou(buf[i]) << "\n";
 
 		assert(buf[i] == ESCAPE);
 		assert(++i < len);
@@ -121,28 +126,94 @@ void parse(char *buf, unsigned len, uint32_t flags)
 				break;
 			case 'a':
 				assert(++i < len);
-				std::cerr << "switch dynamic command mode: " << static_cast<uint32_t>(buf[i]) << "\n";
+				std::cerr << "switch dynamic command mode: " << ctou(buf[i]) << "\n";
 				break;
 			case 'z':
 				std::cerr << "print information:\n";
 				assert(i + 10 < len);
-				for (index = 1; index <= 10; ++index)
-					std::cerr << "\t" << static_cast<uint32_t>(static_cast<uint8_t>(buf[i + index])) << "\n";
+				std::cerr
+					<< "\t" << "used flags " << std::boolalpha
+					<< "media_type=" << static_cast<bool>(buf[i + 1] & 0x02)
+					<< ", media_width=" << static_cast<bool>(buf[i + 1] & 0x04)
+					<< ", media_length=" << static_cast<bool>(buf[i + 1] & 0x08)
+					<< ", unused:print_quality_priority=" << static_cast<bool>(buf[i + 1] & 0x40)
+					<< ", recovery_always_on=" << static_cast<bool>(buf[i + 1] & 0x80) << std::noboolalpha << "\n";
+
+				std::cerr << "\tmedia_type = ";
+				switch (ctou(buf[i + 2])) {
+					case 0x00: std::cerr << "Laminated/Non-laminated tape\n"; break;
+					case 0x11: std::cerr << "Heat-Shrink Tube (HS 2:1)\n"; break;
+					case 0x17: std::cerr << "Heat-Shrink Tube (HS 3:1)\n"; break;
+					case 0x13: std::cerr << "Fle tape\n"; break;
+					case 0xff: std::cerr << "Incompatible tape\n"; break;
+				}
+
+				std::cerr << "\tmedia_width = 0x" << std::hex << std::setfill('0') << std::setw(2) << ctou(buf[i + 3]) << "\n";
+				std::cerr << "\tmedia_length = 0x" << std::hex << std::setfill('0') << std::setw(2) << ctou(buf[i + 4]) << "\n";
+				std::cerr << "\traster number = 0x";
+				printHex(buf[i + 8]);
+				printHex(buf[i + 7]);
+				printHex(buf[i + 6]);
+				printHex(buf[i + 5]);
+				std::cerr << "\n";
+
+				switch (buf[i + 9]) {
+					case 0: std::cerr << "\tstarting page\n"; break;
+					case 1: std::cerr << "\tother pages\n"; break;
+					case 2: std::cerr << "\tlast page\n"; break;
+				}
+
+				// buf[i + 10] == 0
+
 				i += 10;
 				break;
 			case 'M':
 				assert(++i < len);
 				std::cerr << "various mode settings:\n";
-				std::cerr << "\t" << static_cast<uint32_t>(buf[i]) << "\n";
+				if (buf[i] & 0x40)
+					std::cerr << "\tauto cut\n";
+				if (buf[i] & 0x80)
+					std::cerr << "\tmirror printing\n";
 				break;
 			case 'K':
 				assert(++i < len);
-				std::cerr << "advanced mode settings: " << static_cast<uint32_t>(buf[i]) << "\n";
+				std::cerr << "advanced mode settings:\n";
+
+				if (buf[i] & 0x01)
+					std::cerr << "\tdraft printing\n";
+				else
+					std::cerr << "\tnormal printing\n";
+
+				if (buf[i] & 0x04)
+					std::cerr << "\thalf cut on\n";
+				else
+					std::cerr << "\thalf cut off\n";
+
+				if (buf[i] & 0x08)
+					std::cerr << "\tchain printing off\n";
+				else
+					std::cerr << "\tchain printing on\n";
+
+				if (buf[i] & 0x10)
+					std::cerr << "\tspecial tape (no cutting) on\n";
+				else
+					std::cerr << "\tspecial tape (no cutting) off\n";
+
+				if (buf[i] & 0x40)
+					std::cerr << "\thigh-resolution printing\n";
+				else
+					std::cerr << "\tnormal printing\n";
+
+				if (buf[i] & 0x80)
+					std::cerr << "\tno buffer clearing while printing on\n";
+				else
+					std::cerr << "\tno buffer clearing while printing off\n";
+
 				break;
 			case 'd':
 				assert(++i < len);
 				assert(++i < len);
-				std::cerr << "specify margin amount: " << static_cast<uint32_t>(buf[i - 1]) + static_cast<uint32_t>(buf[i]) * 256  << " (margin amount - dots)\n";
+				std::cerr << "specify margin amount: " << std::dec << static_cast<uint32_t>(buf[i - 1]) + static_cast<uint32_t>(buf[i]) * 256  << " (margin amount - dots)\n";
 				break;
 			case 'A':
 				assert(++i < len);
