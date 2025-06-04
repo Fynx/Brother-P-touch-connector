07f0f975dc070fbd6d2aa99d4806eaf07ab49749
Piotr Majcherczyk <pmv@mm.st>
2025-06-04 19:03:08 +0200

    Improve access via script

--- manage.py
+++ manage.py
@@ -3,50 +3,31 @@
 import argparse
 import os
 import subprocess
-from sys import stdin
 from time import sleep
 
 
-def parse_args():
-	parser = argparse.ArgumentParser()
-
-	parser.add_argument("--initialise", action="store_true")
-	parser.add_argument("--device-path", "-d", required=False, default="/dev/usb/lp1")
-	parser.add_argument("--tape-colour", required=True)
-	parser.add_argument("--tape-width", required=True)
-	parser.add_argument("--tape-type", required=True)
-	parser.add_argument("--text-colour", required=True)
-	parser.add_argument("--image", "-i", required=True, help="use 'test' for test page printing")
-	parser.add_argument("--compression", required=False, choices=["no compression", "tiff"], default="tiff")
-	parser.add_argument("--copies", required=False, default=1, type=int)
-	parser.add_argument("--set-length-margin", required=False, type=int, default=14)
-	parser.add_argument("--no-auto-cut", action="store_true")
-	parser.add_argument("--no-half-cut", action="store_true")
-	parser.add_argument("--chain-printing", action="store_true")
-	parser.add_argument("--img-scale-up", action="store_true", help="scales the image up if it's way too small")
-	parser.add_argument("--img-center", action="store_true", help="centers the image if it's too small")
-	parser.add_argument("--img-scale-down", action="store_true", help="scales the image down if it's too big")
-
-	return parser.parse_args()
-
-
-def request_status(device_path, attempts=5, wait=1):
+def request_status(args, attempts=5, wait=1):
 	for i in range(0, attempts):
-		request_status_cmd = ["./make_request", "status", "-o", device_path]
-		print(" ".join(request_status_cmd))
+		request_status_cmd = ["./make_request", "status", "-o", args.device_path]
+		if args.verbose:
+			print(" ".join(request_status_cmd))
 		subprocess.check_output(request_status_cmd)
 
 		sleep(wait)
 
-		read_status_cmd = ["./read_status", device_path]
+		read_status_cmd = ["./read_status", args.device_path]
+		error = None
 		try:
-			print(" ".join(read_status_cmd))
-			output = subprocess.check_output(read_status_cmd).decode("utf-8").strip().split("\n")
+			if args.verbose:
+				print(" ".join(read_status_cmd))
+			output = subprocess.check_output(read_status_cmd, stderr=subprocess.STDOUT).decode("utf-8").strip().split("\n")
 			break
-		except subprocess.CalledProcessError:
-			print("failed")
+		except subprocess.CalledProcessError as e:
+			error = e.output.decode().strip()
+			if args.verbose:
+				print("failed")
 	else:
-		raise Exception("Failed to read printer status")
+		raise Exception(f"Failed to read printer status: '{error}'")
 
 	return {e[0].strip(): ":".join(e[1:]).strip() for line in output if (e := line.split(":"))}
 
@@ -59,29 +40,30 @@ def verify_args(args, status):
 	if status["phase"] != "editing state":
 		raise Exception("Printer must be in 'editing state' phase")
 	if args.tape_width not in status["media width"].split(" / "):
-		raise Exception(f"Mismatched tape width: {args.tape_width}")
+		raise Exception(f"Mismatched tape width: {args.tape_width}, expected {status['media width']}")
 	if args.tape_type != status["media type"]:
-		raise Exception(f"Mismatched tape type: {args.tape_type}")
+		raise Exception(f"Mismatched tape type: {args.tape_type}, expected {status['media type']}")
 	if args.tape_colour != status["tape colour"]:
-		raise Exception(f"Mismatched tape colour: {args.tape_colour}")
+		raise Exception(f"Mismatched tape colour: {args.tape_colour}, expected {status['tape colour']}")
 	if args.text_colour != status["text colour"]:
-		raise Exception(f"Mismatched text colour: {args.text_colour}")
+		raise Exception(f"Mismatched text colour: {args.text_colour}, expected {status['text colour']}")
 
 
 def make_request(args):
-	request_path = "/tmp/request.prn"
-	try:
-		os.remove(request_path)
-	except FileNotFoundError:
-		pass
+	if args.remove_output:
+		try:
+			print(f"Removing {args.output_path}")
+			os.remove(args.output_path)
+		except FileNotFoundError:
+			pass
 
-	subprocess.check_output(["./make_request", "initialise", "-o", request_path])
-	subprocess.check_output(["./make_request", "status", "-o", request_path])
+	subprocess.check_output(["./make_request", "initialise", "-o", args.output_path])
+	subprocess.check_output(["./make_request", "status", "-o", args.output_path])
 
 	command = [
 		"./make_request", "print",
 		"-i", f"'{args.image}'",
-		"-o", f"'{request_path}'",
+		"-o", f"'{args.output_path}'",
 		"--copies", str(args.copies),
 		"--compression", "tiff",
 		"--tape-type", f"'{args.tape_type}'",
@@ -101,25 +83,59 @@ def make_request(args):
 	if args.img_center:
 		command.append("--center")
 
-	print(" ".join(command))
+	if args.verbose:
+		print(" ".join(command))
 	subprocess.check_output(command)
+
+	if args.output_path != args.device_path:
+		print(f"Write request to: {args.output_path}")
+
+
+def parse_args():
+	parser = argparse.ArgumentParser()
+	subparsers = parser.add_subparsers(dest="group", required=True)
+
+	parser_status = subparsers.add_parser("status")
+	parser_status.add_argument("--verbose", "-v", action="store_true")
+	parser_status.add_argument("--device-path", "-d", required=True, default="/dev/usb/lp1", help="file to communicate status request and response with")
+
+	parser_print = subparsers.add_parser("print")
+	parser_print.add_argument("--verbose", "-v", action="store_true")
+	parser_print.add_argument("--device-path", "-d", required=True, help="file to communicate status request and response with")
+	parser_print.add_argument("--output-path", "-o", required=False, default="/tmp/request.prn", help="file to APPEND print request to, for practical purposes same as device path")
+	parser_print.add_argument("--remove-output", action="store_true", help="removes the output file before creation")
+	parser_print.add_argument("--tape-colour", required=True)
+	parser_print.add_argument("--tape-width", required=True)
+	parser_print.add_argument("--tape-type", required=True)
+	parser_print.add_argument("--text-colour", required=True)
+	parser_print.add_argument("--image", "-i", required=True, help="use 'test' for test page printing")
+	parser_print.add_argument("--compression", required=False, choices=["no compression", "tiff"], default="tiff")
+	parser_print.add_argument("--copies", required=False, default=1, type=int)
+	parser_print.add_argument("--set-length-margin", required=False, type=int, default=14)
+	parser_print.add_argument("--no-auto-cut", action="store_true")
+	parser_print.add_argument("--no-half-cut", action="store_true")
+	parser_print.add_argument("--chain-printing", action="store_true")
+	parser_print.add_argument("--img-scale-up", action="store_true", help="scales the image up if it's way too small")
+	parser_print.add_argument("--img-center", action="store_true", help="centers the image if it's too small")
+	parser_print.add_argument("--img-scale-down", action="store_true", help="scales the image down if it's too big")
+
+	return parser.parse_args()
 
 
 def main():
 	args = parse_args()
 
-	if args.initialise:
-		subprocess.check_output(["./make_request", "initialise", "-o", args.device_path])
+	status = request_status(args)
 
-	status = request_status(args.device_path)
+	if args.group == "status" or args.verbose:
+		print("")
+		for k, v in status.items():
+			print(f"{k}: {v}")
+		print("")
 
-	print("")
-	for k, v in status.items():
-		print(f"{k}: {v}")
-	print("")
-
-	verify_args(args, status)
-	make_request(args)
+	if args.group == "print":
+		verify_args(args, status)
+		make_request(args)
 
 
 if __name__ == "__main__":
